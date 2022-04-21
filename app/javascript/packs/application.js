a4c55472750d3e4de3434ea36046d6b2e1f79e79
annayamamoto <anxxx.014@icloud.com>
2022-04-22 08:50:09 +0900

    comitJQueryAdd

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,9 +11,11 @@ import '@fortawesome/fontawesome-free/js/all';
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
+require('jquery')
 require("channels")
 
 require("src/profile_image_upload");
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
