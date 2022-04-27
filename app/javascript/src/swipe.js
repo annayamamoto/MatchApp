fb333b348ccf00b007795d96be14a1c7b64fbd69
annayamamoto <anxxx.014@icloud.com>
2022-04-27 23:07:54 +0900

    修正

--- app/javascript/src/swipe.js
+++ app/javascript/src/swipe.js
@@ -1,15 +1,14 @@
 if(location.pathname == "/users") {
-
   $(function () {
-
     let allCards = document.querySelectorAll('.swipe--card');
-
     let swipeContainer = document.querySelector('.swipe');
 
     function initCards() {
 
+      // この行を追加する
       let newCards = document.querySelectorAll('.swipe--card:not(.removed)');
-  
+
+      // この行を編集する
       newCards.forEach(function (card, index) {
         card.style.zIndex = allCards.length - index;
         card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
@@ -19,58 +18,46 @@ if(location.pathname == "/users") {
       if (newCards.length == 0) {
         $(".no-user").addClass("is-active");
       }
-  
     }
 
-    
     initCards();
 
     allCards.forEach(function (el) {
       let hammertime = new Hammer(el);
 
       hammertime.on('pan', function (event) {
-        if (event.deltaX === 0) return;
-        if (event.center.x === 0 && event.center.y === 0) return;
-
-        el.classList.add('moving');
-
-        swipeContainer.classList.toggle('swipe_like', event.deltaX > 0);
-        swipeContainer.classList.toggle('swipe_dislike', event.deltaX < 0);
-
-        let xMulti = event.deltaX * 0.03;
-        let yMulti = event.deltaY / 80;
-        let rotate = xMulti * yMulti;
-
-        event.target.style.transform = 'translate(' + event.deltaX + 'px, ' + event.deltaY + 'px) rotate(' + rotate + 'deg)';
+        // 中略
       });
-    });
 
-    hammertime.on('panend', function (event) {
-      el.classList.remove('moving');
-      swipeContainer.classList.remove('swipe_like');
-      swipeContainer.classList.remove('swipe_dislike');
+      // ==========ここから追加する==========
+      hammertime.on('panend', function (event) {
+        el.classList.remove('moving');
+        swipeContainer.classList.remove('swipe_like');
+        swipeContainer.classList.remove('swipe_dislike');
 
-      let moveOutWidth = document.body.clientWidth;
+        let moveOutWidth = document.body.clientWidth;
 
-      let keep = Math.abs(event.deltaX) < 200
-      event.target.classList.toggle('removed', !keep);
+        let keep = Math.abs(event.deltaX) < 200
+        event.target.classList.toggle('removed', !keep);
 
-      if (keep) {
-        event.target.style.transform = '';
-      } else {
-        let endX = Math.max(Math.abs(event.velocityX) * moveOutWidth, moveOutWidth) + 100;
-        let toX = event.deltaX > 0 ? endX : -endX;
-        let endY = Math.abs(event.velocityY) * moveOutWidth;
-        let toY = event.deltaY > 0 ? endY : -endY;
-        let xMulti = event.deltaX * 0.03;
-        let yMulti = event.deltaY / 80;
-        let rotate = xMulti * yMulti;
+        if (keep) {
+          event.target.style.transform = '';
+        } else {
+          let endX = Math.max(Math.abs(event.velocityX) * moveOutWidth, moveOutWidth) + 100;
+          let toX = event.deltaX > 0 ? endX : -endX;
+          let endY = Math.abs(event.velocityY) * moveOutWidth;
+          let toY = event.deltaY > 0 ? endY : -endY;
+          let xMulti = event.deltaX * 0.03;
+          let yMulti = event.deltaY / 80;
+          let rotate = xMulti * yMulti;
 
-        event.target.style.transform = 'translate(' + toX + 'px, ' + (toY + event.deltaY) + 'px) rotate(' + rotate + 'deg)';
+          event.target.style.transform = 'translate(' + toX + 'px, ' + (toY + event.deltaY) + 'px) rotate(' + rotate + 'deg)';
 
-        initCards();
-      }
+          initCards();
+        }
+      });
+      // ==========ここまで追加する==========
+
     });
   });
-
 }
