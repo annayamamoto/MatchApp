03a6720747151070cc0c95116c7c6ee7c02a7934
annayamamoto <anxxx.014@icloud.com>
2022-04-27 23:45:17 +0900

    遷移修正中

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
+  # sessions: :sessions を追加
   devise_for :users,
-    controllers: { registrations: 'registrations' }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    controllers: {
+      registrations: 'registrations',
+      sessions: :sessions
+    }
+
   root 'top#index'
 
   resources :users, only: [:show, :index]
