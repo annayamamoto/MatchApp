9124ab8da14c454bf4c36bec37f17bb853edc818
annayamamoto <anxxx.014@icloud.com>
2022-04-30 17:47:42 +0900

    comi

--- app/javascript/src/swipe.js
+++ app/javascript/src/swipe.js
@@ -1,17 +1,15 @@
-if(location.pathname == "/users") {
+if (location.pathname == "/users") {
   $(function () {
-    let allCards = document.querySelectorAll('.swipe--card');
-    let swipeContainer = document.querySelector('.swipe');
+    let allCards = document.querySelectorAll(".swipe--card");
+    let swipeContainer = document.querySelector(".swipe");
 
     function initCards() {
+      let newCards = document.querySelectorAll(".swipe--card:not(.removed)");
 
-      // この行を追加する
-      let newCards = document.querySelectorAll('.swipe--card:not(.removed)');
-
-      // この行を編集する
       newCards.forEach(function (card, index) {
         card.style.zIndex = allCards.length - index;
-        card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
+        card.style.transform =
+          "scale(" + (20 - index) / 20 + ") translateY(-" + 30 * index + "px)";
         card.style.opacity = (10 - index) / 10;
       });
 
@@ -25,25 +23,24 @@ if(location.pathname == "/users") {
     allCards.forEach(function (el) {
       let hammertime = new Hammer(el);
 
-      hammertime.on('pan', function (event) {
-        // 中略
-      });
-
-      // ==========ここから追加する==========
-      hammertime.on('panend', function (event) {
-        el.classList.remove('moving');
-        swipeContainer.classList.remove('swipe_like');
-        swipeContainer.classList.remove('swipe_dislike');
+      hammertime.on("panend", function (event) {
+        el.classList.remove("moving");
+        swipeContainer.classList.remove("swipe_like");
+        swipeContainer.classList.remove("swipe_dislike");
 
         let moveOutWidth = document.body.clientWidth;
 
-        let keep = Math.abs(event.deltaX) < 200
-        event.target.classList.toggle('removed', !keep);
+        let keep = Math.abs(event.deltaX) < 200;
+        event.target.classList.toggle("removed", !keep);
+
+        let reaction = event.deltaX > 0 ? "like" : "dislike";
 
         if (keep) {
-          event.target.style.transform = '';
+          event.target.style.transform = "";
         } else {
-          let endX = Math.max(Math.abs(event.velocityX) * moveOutWidth, moveOutWidth) + 100;
+          let endX =
+            Math.max(Math.abs(event.velocityX) * moveOutWidth, moveOutWidth) +
+            100;
           let toX = event.deltaX > 0 ? endX : -endX;
           let endY = Math.abs(event.velocityY) * moveOutWidth;
           let toY = event.deltaY > 0 ? endY : -endY;
@@ -51,41 +48,68 @@ if(location.pathname == "/users") {
           let yMulti = event.deltaY / 80;
           let rotate = xMulti * yMulti;
 
-          event.target.style.transform = 'translate(' + toX + 'px, ' + (toY + event.deltaY) + 'px) rotate(' + rotate + 'deg)';
+          postReaction(el.id, reaction);
+
+          event.target.style.transform =
+            "translate(" +
+            toX +
+            "px, " +
+            (toY + event.deltaY) +
+            "px) rotate(" +
+            rotate +
+            "deg)";
 
           initCards();
         }
       });
-      // ==========ここまで追加する==========
-
     });
 
-    function createButtonListener(reaction) {
+    function postReaction(user_id, reaction) {
+      $.ajax({
+        url: "reactions.json",
+        type: "POST",
+        datatype: "json",
+        data: {
+          user_id: user_id,
+          reaction: reaction,
+        },
+      }).done(function () {
+        console.log("done!");
+      });
+    }
 
-      let cards = document.querySelectorAll('.swipe--card:not(.removed)');
+    function createButtonListener(reaction) {
+      let cards = document.querySelectorAll(".swipe--card:not(.removed)");
 
       if (!cards.length) return false;
-    
+
       let moveOutWidth = document.body.clientWidth * 2;
-    
+
       let card = cards[0];
-      card.classList.add('removed');
-    
+
+      let user_id = card.id;
+
+      postReaction(user_id, reaction);
+
+      card.classList.add("removed");
+
       if (reaction == "like") {
-        card.style.transform = 'translate(' + moveOutWidth + 'px, -100px) rotate(-30deg)';
+        card.style.transform =
+          "translate(" + moveOutWidth + "px, -100px) rotate(-30deg)";
       } else {
-        card.style.transform = 'translate(-' + moveOutWidth + 'px, -100px) rotate(30deg)';
+        card.style.transform =
+          "translate(-" + moveOutWidth + "px, -100px) rotate(30deg)";
       }
-    
+
       initCards();
     }
 
-    $('#like').on('click', function() {
+    $("#like").on("click", function () {
       createButtonListener("like");
-    })
+    });
 
-    $('#dislike').on('click', function() {
+    $("#dislike").on("click", function () {
       createButtonListener("dislike");
-    })
+    });
   });
 }
